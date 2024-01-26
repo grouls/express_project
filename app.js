b1fd80b3ede05041a69f5a5eebf9275a15b3a4f4
grouls <roulstone.gary@gmail.com>
2024-01-26 16:43:42 +0000

    add and display users

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!!!')
+let users = [
+  { id: 1, name: 'Gary', age: 30 },
+  { id: 2, name: 'Michael', age: 33 },
+];
+
+app.use(bodyParser.json());
+
+// GET users
+app.get('/api/users', (req, res) => res.json(users));
+
+// POST a user
+app.post('/api/users', (req, res) => {
+  const newUser = req.body;
+  newUser.id = users.length + 1;
+
+  users.push(newUser);
+
+  res.status(201).json(newUser);
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-});
+app.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
